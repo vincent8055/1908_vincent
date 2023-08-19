4328d9ed4a01f1ff1c07f8b11bf842b08c4f2723
vincent8055 <vincent@mychen.net>
2023-08-19 12:23:16 +0800

    access key

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports.handler = async (event) => {
       body: JSON.stringify(
         {
           message: "Your function executed successfully!",
+          access_key: process.env.ACCESS_KEY
         },
         null,
         2
